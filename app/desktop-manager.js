6825d89b4fa94e10c64510e3a290426afd2f855f
icookie-tea <1024494987@qq.com>
2026-08-02 19:13:13 +0800

    refactor: extract GridLayout (monitor geometry) from DesktopManager
    desktop-manager.js 1620 -> 1547 lines; new app/grid-layout.js (111):
    - dbusAdvertiseUpdate (subscribes to extension desktopGeometry action)
    - updateGridWindows (diff + recreate grids; DM keeps a one-line
      forwarding method because ding.js calls it on the manager)
    - createGridWindows (per-monitor DesktopGrid creation)
    - registered in app/meson.build

--- app/desktop-manager.js
+++ app/desktop-manager.js
@@ -26,7 +26,6 @@ const Gio = imports.gi.Gio;
 const Adw = imports.gi.Adw;
 
 const FileItem = imports['file-item'];
-const DesktopGrid = imports['desktop-grid'];
 const DesktopIconsUtil = imports['desktop-icons-util'];
 const Prefs = imports.preferences;
 const Enums = imports.enums;
@@ -39,6 +38,7 @@ const AutoAr = imports['auto-ar'];
 const SignalManager = imports['signal-manager'];
 const DesktopMenu = imports['desktop-menu'];
 const DesktopMonitor = imports['desktop-monitor'];
+const GridLayout = imports['grid-layout'];
 const FileChangesQueue = imports['file-changes-queue'];
 const Constants = imports.constants;
 const DebugLog = imports.log;
@@ -152,6 +152,7 @@ var DesktopManager = class {
         this._asDesktop = asDesktop;
         this._desktopList = desktopList;
         this._desktops = [];
+        this._gridLayout = new GridLayout.GridLayout(this);
     }
     _initFileMonitoring() {
         this._monitor = new DesktopMonitor.DesktopMonitor(this);
@@ -327,7 +328,7 @@ var DesktopManager = class {
             });
         }
         if (this._asDesktop) {
-            this._dbusAdvertiseUpdate();
+            this._gridLayout.dbusAdvertiseUpdate();
         }
     }
 
@@ -343,88 +344,14 @@ var DesktopManager = class {
         return updateFileList;
     }
 
-    _dbusAdvertiseUpdate() {
-        DBusUtils.extensionControl.connect('action-state-changed', (actionGroup, actionName, data) => {
-            if (actionName == 'desktopGeometry') {
-                this.updateGridWindows(data.recursiveUnpack());
-            }
-        });
-        DBusUtils.extensionControl.connect('action-added', (actionGroup, actionName) => {
-            // this signal allows us to know when the action is available and we can read the initial value
-            if (actionName == 'desktopGeometry') {
-                let data = DBusUtils.extensionControl.get_action_state('desktopGeometry');
-                this.updateGridWindows(data.recursiveUnpack());
-            }
-        });
-        // This is required to trigger the 'action-added' signal
-        DBusUtils.extensionControl.list_actions();
-    }
+
+
+
+
+
 
     updateGridWindows(newdesktoplist) {
-        let newPrimaryIndex = -1;
-        if ((newdesktoplist.length > 0) && ('primaryMonitor' in newdesktoplist[0])) {
-            newPrimaryIndex = newdesktoplist[0].primaryMonitor;
-            this._primaryIndex = newPrimaryIndex;
-        }
-        if (this._desktopList && (newdesktoplist.length == this._desktopList.length)) {
-            let gridschanged = [];
-            for (let index = 0; index < newdesktoplist.length; index++) {
-                let area = newdesktoplist[index];
-                let area2 = this._desktopList[index];
-                if ((area.x != area2.x) ||
-                    (area.y != area2.y) ||
-                    (area.width != area2.width) ||
-                    (area.height != area2.height) ||
-                    (area.scaleFactor !== area2.scaleFactor) ||
-                    (area.monitorIndex != area2.monitorIndex)) {
-                    gridschanged.push(index);
-                    continue;
-                }
-                if ((area.marginTop != area2.marginTop) ||
-                    (area.marginBottom != area2.marginBottom) ||
-                    (area.marginLeft != area2.marginLeft) ||
-                    (area.marginRight != area2.marginRight)) {
-                    if (!gridschanged.includes(index)) {
-                        gridschanged.push(index);
-                    }
-                }
-            }
-            if (gridschanged.length == 0) {
-                if (this._primaryIndex < this._desktopList.length) {
-                    this._primaryScreen = this._desktopList[this._primaryIndex];
-                } else {
-                    this._primaryScreen = null;
-                }
-                return;
-            }
-        }
-        this._desktopList = newdesktoplist;
-        if (this._primaryIndex < this._desktopList.length) {
-            this._primaryScreen = this._desktopList[this._primaryIndex];
-        } else {
-            this._primaryScreen = null;
-        }
-        this._createGridWindows();
-        this._updateDesktopSafe('grid update');
-    }
-
-    _createGridWindows() {
-        this._removeAllFilesFromGrids();
-        for (let desktop of this._desktops) {
-            desktop.destroy();
-        }
-        this._desktops = [];
-        for (let desktopIndex in this._desktopList) {
-            let desktop = this._desktopList[desktopIndex];
-            let desktopName;
-            if (this._asDesktop) {
-                // this name must match the one used in emulateX11WindowType
-                desktopName = `Desktop Icons ${desktop.monitorIndex + 1}`;
-            } else {
-                desktopName = `DING ${desktop.monitorIndex + 1}`;
-            }
-            this._desktops.push(new DesktopGrid.DesktopGrid(this, desktopName, desktop, this._asDesktop));
-        }
+        this._gridLayout.updateGridWindows(newdesktoplist);
     }
 
     get selectColor() {
---
+++ app/grid-layout.js
@@ -0,0 +1,111 @@
+/* DING: Desktop Icons New Generation for GNOME Shell
+ *
+ * Copyright (C) 2019 Sergio Costas (rastersoft@gmail.com)
+ * Based on code original (C) Carlos Soriano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+'use strict';
+const DBusUtils = imports['dbus-utils'];
+const DesktopGrid = imports['desktop-grid'];
+
+/* Monitor geometry management: subscribes to the extension's D-Bus
+ * desktopGeometry action, diffs monitor areas, recreates the per-monitor
+ * DesktopGrid windows and keeps _primaryScreen up to date. State lives on
+ * the DesktopManager (this._dm). */
+var GridLayout = class {
+    constructor(desktopManager) {
+        this._dm = desktopManager;
+    }
+
+    updateGridWindows(newdesktoplist) {
+        let newPrimaryIndex = -1;
+        if ((newdesktoplist.length > 0) && ('primaryMonitor' in newdesktoplist[0])) {
+            newPrimaryIndex = newdesktoplist[0].primaryMonitor;
+            this._dm._primaryIndex = newPrimaryIndex;
+        }
+        if (this._dm._desktopList && (newdesktoplist.length == this._dm._desktopList.length)) {
+            let gridschanged = [];
+            for (let index = 0; index < newdesktoplist.length; index++) {
+                let area = newdesktoplist[index];
+                let area2 = this._dm._desktopList[index];
+                if ((area.x != area2.x) ||
+                    (area.y != area2.y) ||
+                    (area.width != area2.width) ||
+                    (area.height != area2.height) ||
+                    (area.scaleFactor !== area2.scaleFactor) ||
+                    (area.monitorIndex != area2.monitorIndex)) {
+                    gridschanged.push(index);
+                    continue;
+                }
+                if ((area.marginTop != area2.marginTop) ||
+                    (area.marginBottom != area2.marginBottom) ||
+                    (area.marginLeft != area2.marginLeft) ||
+                    (area.marginRight != area2.marginRight)) {
+                    if (!gridschanged.includes(index)) {
+                        gridschanged.push(index);
+                    }
+                }
+            }
+            if (gridschanged.length == 0) {
+                if (this._dm._primaryIndex < this._dm._desktopList.length) {
+                    this._dm._primaryScreen = this._dm._desktopList[this._dm._primaryIndex];
+                } else {
+                    this._dm._primaryScreen = null;
+                }
+                return;
+            }
+        }
+        this._dm._desktopList = newdesktoplist;
+        if (this._dm._primaryIndex < this._dm._desktopList.length) {
+            this._dm._primaryScreen = this._dm._desktopList[this._dm._primaryIndex];
+        } else {
+            this._dm._primaryScreen = null;
+        }
+        this._dm._createGridWindows();
+        this._dm._updateDesktopSafe('grid update');
+    }
+    createGridWindows() {
+        this._dm._removeAllFilesFromGrids();
+        for (let desktop of this._dm._desktops) {
+            desktop.destroy();
+        }
+        this._dm._desktops = [];
+        for (let desktopIndex in this._dm._desktopList) {
+            let desktop = this._dm._desktopList[desktopIndex];
+            let desktopName;
+            if (this._dm._asDesktop) {
+                // this name must match the one used in emulateX11WindowType
+                desktopName = `Desktop Icons ${desktop.monitorIndex + 1}`;
+            } else {
+                desktopName = `DING ${desktop.monitorIndex + 1}`;
+            }
+            this._dm._desktops.push(new DesktopGrid.DesktopGrid(this, desktopName, desktop, this._dm._asDesktop));
+        }
+    }
+    dbusAdvertiseUpdate() {
+        DBusUtils.extensionControl.connect('action-state-changed', (actionGroup, actionName, data) => {
+            if (actionName == 'desktopGeometry') {
+                this.updateGridWindows(data.recursiveUnpack());
+            }
+        });
+        DBusUtils.extensionControl.connect('action-added', (actionGroup, actionName) => {
+            // this signal allows us to know when the action is available and we can read the initial value
+            if (actionName == 'desktopGeometry') {
+                let data = DBusUtils.extensionControl.get_action_state('desktopGeometry');
+                this.updateGridWindows(data.recursiveUnpack());
+            }
+        });
+        // This is required to trigger the 'action-added' signal
+        DBusUtils.extensionControl.list_actions();
+    }};
